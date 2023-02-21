0ff8712865601e7cbdc6d690aedb8a342ca10d3c
yosr <yosrabroug044@gmail.com>
2023-02-21 19:54:05 +0100

    fourth commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,37 @@
-import React from "react"
-import './App.css'
-import {StreamChat} from 'stream-chat'
-import {Chat } from 'stream-chat-react'
-// import Cookies from 'universal-cookie'
-import ChannelListContainer from './components/ChannelListContainer'
-import ChannelContainer from './components/ChannelContainer'
-import Auth from './components/Auth'
+import React from "react";
+import "./App.css";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import ChannelListContainer from "./components/ChannelListContainer";
+import ChannelContainer from "./components/ChannelContainer";
+import Auth from "./components/Auth";
 const apiKey = "527u67ju3axr";
 const client = StreamChat.getInstance(apiKey);
-const authToken = false;
+const cookies = new Cookies();
+const authToken = cookies.get("token");
 
-export default function App(){
+if (authToken) {
+  client.connectUser({
+    id: cookies.get("userId"),
+    name: cookies.get("username"),
+    fullName: cookies.get("fullName"),
+    image: cookies.get("avatarURL"),
+    hashedPassword: cookies.get("hashedPassword"),
+    phoneNumber: cookies.get("phoneNumber"),
+  }, authToken)
+ //connect user and fetch msgs
+}
 
-  if (!authToken) return <Auth/>
+export default function App() {
+  if (!authToken) return <Auth />;
 
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
-        <ChannelListContainer/>
-        <ChannelContainer/> 
+        <ChannelListContainer />
+        <ChannelContainer />
       </Chat>
     </div>
-  )
+  );
 }
-
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,3 +1,149 @@
-export default function Auth(){
-    return(<div></div>)
+import { useState } from "react";
+import Cookies from "universal-cookie";
+import axios from "axios";
+import signin from "../assets/signin.png";
+
+let initialState = {
+  fullName: "",
+  username: "",
+  password: "",
+  confirmPassword: "",
+  phoneNumber: "",
+  avatarUrl: "",
+};
+
+const cookies = new Cookies();
+
+export default function Auth() {
+  const [form, setForm] = useState(initialState);
+  const [isSignup, setIsSignup] = useState(true);
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const { fullName, username, password, phoneNumber, avatarUrl } = form;
+
+    const URL = "http;//localhost:5000/auth";
+    const {
+      data: { token, userId, hashedPassword },
+    } = await axios.post(`${URL}/${isSignup ? "signup" : "login"}`, {
+      username,
+      password,
+      fullName,
+      phoneNumber,
+      avatarUrl,
+    });
+    cookies.set("token", token);
+    cookies.set("username", username);
+    cookies.set("fullName", fullName);
+    cookies.set("userId", userId);
+
+    if (isSignup) {
+      cookies.set("phoneNumber", phoneNumber);
+      cookies.set("avatarURL", avatarURL);
+      cookies.set("hashedPassword", hashedPassword);
+    }
+    // store data in cookies
+    window.location.reload();
+    //  reload browser so auth changes value in app.jsx
+  }
+  function switchMode() {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+  }
+  return (
+    <div className="auth__form-container">
+      <div className="auth__form-container_fields">
+        <div className="auth__form-container_fields-content">
+          <p>{isSignup ? "Sign Up" : "Sign In"}</p>
+          <form onSubmit={handleSubmit}>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="fullname">Full Name</label>
+                <input
+                  name="fullName"
+                  type="text"
+                  placeholder="Full Name"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            {/* not in signup bc it shows up every time we sign in too */}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="username">User Name</label>
+              <input
+                name="username"
+                type="text"
+                placeholder="User Name"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input
+                  name="phoneNumber"
+                  type="text"
+                  placeholder="Phone Number"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="avatarURL">Avatar URL</label>
+                <input
+                  name="avatarURL"
+                  type="text"
+                  placeholder="Avatar URL"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="confirmpassword">Confirm Password</label>
+                <input
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_button">
+              <button>{isSignup ? "Sign Up" : "Sign In"}</button>
+            </div>
+          </form>
+          <div className="auth__form-container_fields-account">
+            <p>
+              {isSignup ? "Already have an account?" : "Don't have an account?"}
+              <span onClick={switchMode}>
+                {isSignup ? "Sign In" : "Sign Up"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="auth__form-container_image">
+        <img src={signin} />
+      </div>
+    </div>
+  );
 }
